28af917a86cddaa30f8267e5259c499b031d9225
ist1110951 <joao.meneses.santos@tecnico.ulisboa.pt>
2024-11-10 11:34:01 +0000

    add session creating

--- src/components/teacherPage/teacherPage.tsx
+++ src/components/teacherPage/teacherPage.tsx
@@ -1,10 +1,12 @@
 import { TopBar } from "@components/topbar";
+import { SessionManager } from 'react-together';
 
 export default function TeacherPage() {
     return (
         <div>
             <TopBar title={"Teacher"} />
             <h1>Teacher</h1>
+            <SessionManager />
         </div>
     );
 }
